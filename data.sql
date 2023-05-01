cea283e13c3c7c2bb085a5c4b511e5f78804323e
Jessica Hsiang <hsiang.jessica@gmail.com>
2023-05-01 16:00:28 -0700

    Add industries and industries_companies tables + sample data

--- data.sql
+++ data.sql
@@ -1,7 +1,13 @@
 \c biztime
 
-DROP TABLE IF EXISTS invoices;
-DROP TABLE IF EXISTS companies;
+DROP TABLE IF EXISTS invoices
+CASCADE;
+DROP TABLE IF EXISTS companies
+CASCADE;
+DROP TABLE IF EXISTS industries
+CASCADE;
+DROP TABLE IF EXISTS industries_companies
+CASCADE;
 
 CREATE TABLE companies (
     code text PRIMARY KEY,
@@ -19,6 +25,17 @@ CREATE TABLE invoices (
     CONSTRAINT invoices_amt_check CHECK ((amt > (0)::double precision))
 );
 
+CREATE TABLE industries (
+    code text PRIMARY KEY,
+    industry text NOT NULL UNIQUE
+);
+
+CREATE TABLE industries_companies (
+    industry_code TEXT NOT NULL REFERENCES industries,
+    company_code TEXt NOT NULL REFERENCES companies,
+    PRIMARY KEY(industry_code, company_code)
+);
+
 INSERT INTO companies
   VALUES ('apple', 'Apple Computer', 'Maker of OSX.'),
          ('ibm', 'IBM', 'Big blue.');
@@ -28,3 +45,15 @@ INSERT INTO invoices (comp_Code, amt, paid, paid_date)
          ('apple', 200, false, null),
          ('apple', 300, true, '2018-01-01'),
          ('ibm', 400, false, null);
+
+INSERT INTO industries 
+VALUES 
+('acct', 'Accounting'), 
+('it', 'Information Technology'),
+('ai', 'Artificial Intelligence');
+
+INSERT INTO industries_companies
+VALUES 
+('it', 'apple'),
+('it', 'ibm'),
+('ai', 'ibm');
